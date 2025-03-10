4243750e2e858080642196cc7dc7c53be804a1e2
ayukthi32 <ayukthi32@gmail.com>
2025-03-10 16:26:07 +0530

    lesson 20 tuple

---
+++ main.py
@@ -0,0 +1,17 @@
+#activity1
+tuple = (1 , False , 3.4 , 7)
+print(tuple)
+
+tuple1 = ( 1, 2, 3, 4, 5)
+print(tuple1)
+tuple1 = tuple1 + (9,)
+print(tuple1)
+
+tuple2 = (20 , 30 , 40 , 50 , 60 , 40)
+print(tuple2.count(40))
+
+tuplex = (1,2,3,4,5,6,7,8,9,10)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
